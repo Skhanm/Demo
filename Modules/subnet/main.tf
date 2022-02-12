5b11b85aaf75d96c6156ab98d431ab620c7abec8
Skhanm <98329985+Skhanm@users.noreply.github.com>
2022-02-11 20:06:34 -0500

    subnet module

---
+++ Modules/subnet/main.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "web-subnet-1" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_customer_owned_ip_on_launch 
+
+  tags = {
+    Name = var.name_tag
+  }
+}
+
---
+++ Modules/subnet/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {
+  default = ""
+}
+
+variable "cidr_block" {
+    default = ""
+}
+
+variable "availability_zone" {
+    default = ""
+  
+}
+
+variable "map_customer_owned_ip_on_launch" {
+  
+}
+
+variable "name_tag" {
+  
+}
